6e44f93cd714516ee34e52ae52d719d8af60493b
Stefan Micheelsen <sm@intempus.dk>
2024-02-18 21:28:28 +0100

    First demo of album display

--- web/urls.py
+++ web/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import (
     delete_task,
     create_task,
+    display_albums,
     display_tasks,
 )
 
 urlpatterns = [
     path('tasks/', display_tasks, name='display_tasks'),
+    path('albums/', display_albums, name='display_albums'),
     path('tasks/<int:id>/delete/', delete_task, name='delete_task'),
     path('tasks/create/', create_task, name='create_task'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
@@ -8,6 +10,22 @@ def display_tasks(request):
     tasks = Task.objects.all()
     return render(request, 'display_tasks.html', {'tasks': tasks})
 
+
+@dataclass
+class Album:
+    id: int
+    title: str
+    url: str
+
+
+def display_albums(request):
+    test = "https://upload.wikimedia.org/wikipedia/en/8/83/Slayer_-_Show_No_Mercy.jpg"
+    albums = [
+        Album(1, 'Album 1', test),
+        Album(2, 'Album 2', test),
+        Album(3, 'Album 3', test),
+    ]
+    return render(request, 'display_albums.html', {'albums': albums})
 
 @require_http_methods(['DELETE'])
 def delete_task(request, id):
