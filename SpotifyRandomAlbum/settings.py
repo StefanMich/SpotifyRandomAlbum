9af8cace95dde5fa61be5d7d095fc4e887b70266
Stefan Micheelsen <sm@intempus.dk>
2024-03-28 21:23:14 +0100

    Add django-htmx

--- SpotifyRandomAlbum/settings.py
+++ SpotifyRandomAlbum/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
+    "django_htmx",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'SpotifyRandomAlbum.urls'
