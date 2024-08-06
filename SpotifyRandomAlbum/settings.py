db2ff74882e4c9a461f1eb083e48c404ee3b922f
Stefan Micheelsen <sm@intempus.dk>
2024-08-06 23:08:31 +0200

    Log spotipy.oauth2 to stdout

--- SpotifyRandomAlbum/settings.py
+++ SpotifyRandomAlbum/settings.py
@@ -126,3 +126,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'spotipy.oauth2': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
