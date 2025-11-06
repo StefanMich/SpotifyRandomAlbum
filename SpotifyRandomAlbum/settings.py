d90451b7466aea743ff292f968e9262a940c255e
Stefan Micheelsen <sm@intempus.dk>
2025-11-06 21:57:49 +0100

    Staticfiles | Add whitenoise and collect staticfiles

--- SpotifyRandomAlbum/settings.py
+++ SpotifyRandomAlbum/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = '/app/static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,5 +3,8 @@
 # Run migrations
 python manage.py migrate
 
+# Collect static files
+python manage.py collectstatic --no-input
+
 # Start gunicorn
 gunicorn SpotifyRandomAlbum.wsgi:application --bind 0.0.0.0:8000 --workers 3 --timeout 120
